540fa4e31db4d111fa1c087864b74cf6abe929f0
Steven <dombrowskis153@yahoo.com>
2021-09-02 08:31:55 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+ if (feet <= 400){
+   return "This one is on me!"
+ }
+ else if (feet > 2500){
+  return "No can do."
+}
+ else if (feet > 2000){
+ return "I will gladly take your thirty bucks."
+ }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+if(city === "NYC"){
+return "Ok, sounds good."} 
+else if (city != "NYC"){
+  return "No go."
 }
 
-function switchOnCharmFromTip(){
+}
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+if (tip == "generous"){
+  return "Thank you so much."
+}
+else if (tip == "not as generous"){
+  return "Thank you."
+}
+else if (tip == "thanks for everything"){
+  return "Bye."
+}
 }
